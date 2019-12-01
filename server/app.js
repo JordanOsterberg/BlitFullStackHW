65273072c1ed5a698f5fd4a8cebbdbb02253f435
Jordan Osterberg <jordan.osterberg@baysideonline.com>
2019-12-01 10:17:52 -0800

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post("/api/weather", (req, res) => {
+
+});
+
+module.exports = app;
