79e780b7f8484d13216585d4b62d2988e7718bb0
Jordan Osterberg <jordan.osterberg@baysideonline.com>
2019-12-01 13:02:37 -0800

    Prettied the design

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ export default class App extends React.Component {
             temperatureHigh: null,
             temperatureLow: null,
             serverCityName: null,
+            zipCode: null,
             isResultCached: false,
 
             cityInput: null
@@ -32,32 +33,86 @@ export default class App extends React.Component {
                 <div style={{
                     marginTop: 15
                 }}>
-                    {this.state.displayWeatherResult ? <div className="weatherResult">
-                        <p>{this.state.serverCityName} is currently {this.state.temperature}&deg;F. The high is {this.state.temperatureHigh}&deg;F, and the low is {this.state.temperatureLow}&deg;F. {this.state.isResultCached ? "(Cached)" : null}</p>
-                    </div> : null}
-
-                    <input type="text"
-                           placeholder="City Name or ZIP Code"
-                           onChange={this.handleCityInputChange.bind(this)}
-                           onKeyPress={(e) => {
-                               if (e.key === "Enter") {
-                                   this.sendForecastRequest(this.state.cityInput, true)
-                               }
-                           }}
-                    />
-
-                    <button onClick={(e) => {
-                        e.preventDefault();
-                        this.sendForecastRequest(this.state.cityInput, true)
-                    }}>Get Forekast!</button>
-
-                    {this.state.isResultCached ? <button onClick={(e) => {
-                        e.preventDefault();
-                        this.sendForecastRequest(this.state.cityInput, false)
-                    }}>Clear Cache</button> : null}
+                    {this.state.displayWeatherResult ? this.renderForecast() : this.renderInputs()}
                 </div>
             </div>
         );
+    }
+
+    renderInputs() {
+        return <div className="inputHolder">
+            <input type="text"
+                   placeholder="ZIP Code or City Name"
+                   onChange={this.handleCityInputChange.bind(this)}
+                   onKeyPress={(e) => {
+                       if (e.key === "Enter") {
+                           this.sendForecastRequest(this.state.cityInput, true)
+                       }
+                   }}
+            />
+
+            <button className="button" style={{
+                marginTop: 10,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                display: 'block'
+            }} onClick={(e) => {
+                e.preventDefault();
+                this.sendForecastRequest(this.state.cityInput, true)
+            }}>Get Forekast!</button>
+        </div>;
+    }
+
+    renderForecast() {
+        return <div className="weatherResult">
+            <h2 style={{
+                color: 'rgba(0, 0, 0, 0.5)',
+                fontSize: 26,
+                marginBottom: 0
+            }}>{this.state.serverCityName}{this.state.zipCode !== null ? " (" + this.state.zipCode + ")" : null}{this.state.isResultCached ? <p style={{marginLeft: 6, display: 'inline', fontSize: 12, color: 'rgba(0, 0, 0, 0.5)'}}>Cached</p> : null}</h2>
+
+            <h1 style={{
+                fontSize: 48,
+                marginTop: 3,
+                marginBottom: 3
+            }}>{this.state.temperature}&deg;F</h1>
+
+            <h2 style={{
+                color: 'rgba(0, 0, 0, 0.5)',
+                fontSize: 22,
+                marginTop: 3,
+                marginBottom: 0
+            }}>High: {this.state.temperatureHigh}&deg;F</h2>
+
+            <h2 style={{
+                color: 'rgba(0, 0, 0, 0.5)',
+                fontSize: 22,
+                marginBottom: 12,
+                marginTop: 0
+            }}>Low: {this.state.temperatureLow}&deg;F</h2>
+
+            <button style={{display: 'inline'}}
+                    className="button"
+                    onClick={(e) => {
+                        e.preventDefault();
+
+                        const currentState = this.state;
+                        currentState.displayWeatherResult = false;
+                        this.setState(currentState);
+                    }}>
+                Change Location
+            </button>
+
+            <button style={{display: 'inline', marginLeft: 12}}
+                    className="button"
+                    onClick={(e) => {
+                        e.preventDefault();
+
+                        this.sendForecastRequest(this.state.cityInput, false)
+                    }}>
+                Reset Cache
+            </button>
+        </div>;
     }
 
     handleCityInputChange(event) {
@@ -87,6 +142,13 @@ export default class App extends React.Component {
             currentState.temperatureLow = body.low;
             currentState.serverCityName = body.city;
             currentState.isResultCached = body.isCached;
+
+            if (/^\d+$/.test(cityInput)) {
+                currentState.zipCode = cityInput;
+            } else {
+                currentState.zipCode = null;
+            }
+
             this.setState(currentState);
         })
     }
